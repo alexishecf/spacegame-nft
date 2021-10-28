adbe070a7827b42ef07db67cc83cdceefb1d201a
alexishecf <90714480+alexishecf@users.noreply.github.com>
2021-10-28 14:00:17 +0200

    Updated tailwind safelist
    allow dynamic background color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      safelist: ["bg-blue-500", "bg-blue-400", "bg-purple-500", "bg-purple-400", "bg-indigo-500", "bg-indigo-400"],
+    },
+  },
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
